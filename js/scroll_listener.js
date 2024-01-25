32ee1788ec4b6dcb5ee6bafe0031aa6487f3c53e
Mial Lewis <mial_lewis@live.co.uk>
2024-01-25 02:09:24 +0000

    first attempt at using es6 modules

--- js/scroll_listener.js
+++ js/scroll_listener.js
@@ -1,13 +1,17 @@
+import { clickListenerActive } from "./js/skip_button_onclick.js";
 document.addEventListener('scroll', function() {
     var myButton = document.getElementById('NZ_skip_button');
     var referenceObj = document.getElementById('NZ_START');
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var hideAtPosition = referenceObj.offsetTop; // Change this value based on your requirement
-
-    if (scrollPosition > hideAtPosition) {
-        myButton.style.display = 'none';
+    if ( clickListenerActive.bool == false ) {
+        if (scrollPosition > hideAtPosition) {
+            myButton.style.display = 'none';
+        } else {
+            myButton.style.display = 'block';
+        }
+    
     } else {
-        myButton.style.display = 'block';
+        clickListenerActive.bool = false;
     }
 });
-
--- js/skip_button_onclick.js
+++ js/skip_button_onclick.js
@@ -1,7 +1,10 @@
-let myButton = document.getElementById('NZ_skip_button');
-var clickListenerActive = false;
+function passableBool(bool_val) {
+    this.bool = bool_val;
+}
+
+export var clickListenerActive = new passableBool(false);
+var myButton = document.getElementById('NZ_skip_button');
 myButton.addEventListener('click', function() {
     myButton.style.display = 'none';
-    clickListenerActive = true;
+    clickListenerActive.bool = true;
 });
-
