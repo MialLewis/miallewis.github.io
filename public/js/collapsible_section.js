c6e0691bf657886d56f136daf90e46e9d1d8f2c6
Mial Lewis <mial_lewis@live.co.uk>
2024-01-28 22:29:09 +0000

    correct collapsible js

--- public/js/collapsible_section.js
+++ public/js/collapsible_section.js
@@ -1,13 +1,13 @@
 applyListenerToAllElements("section-header-container-up", false);
-applyListenerToAllElements("section-header-container-down", false);
+applyListenerToAllElements("section-header-container-down", true);
 
 function applyListenerToAllElements(element, active){
   var coll = document.getElementsByClassName(element);
   var i;
-  if(active){
-    coll[i].classList.toggle("active");
-  }
   for (i = 0; i < coll.length; i++) {
+    if(active){
+      coll[i].classList.toggle("active");
+    }
     coll[i].addEventListener("click", function() {
       var content = this.nextElementSibling;
       manipulateContent(this, content);
