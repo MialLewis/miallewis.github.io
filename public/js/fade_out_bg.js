2d92ffe6c953ac34f2cfb7decb6b7c8b9bfe36fa
Mial Lewis <mial_lewis@live.co.uk>
2024-01-30 21:03:35 +0000

    switch links prior to page hop

--- public/js/fade_out_bg.js
+++ public/js/fade_out_bg.js
@@ -1,10 +1,13 @@
 $(function(){
     $('.home-link').click(function(){
         let homeLink = $('.home-link');
-        homeLink.text("RESUME");
-        $('.sep-placeholder').text("/");
-        $('.travel-placeholder').text("TRAVEL");
-        $('.header').width("55%");
+        homeLink.text('RESUME');
+        homeLink.on('click', function() {
+            window.location = 'resume.html';
+        });
+        $('.sep-placeholder').text('/');
+        $('.travel-placeholder').text('TRAVEL');
+        $('.header').width('55%');
         let baseBg = $('.base-bg');
         baseBg.on('animationend', function() {
             window.location = 'index.html';
