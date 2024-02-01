a118996038104e6aef596dfb3b9ea2e34229c8b6
Mial Lewis <mial_lewis@live.co.uk>
2024-02-01 07:18:24 +0000

    fix opacity of button

--- public/js/skip_button_onclick.js
+++ public/js/skip_button_onclick.js
@@ -7,4 +7,6 @@ var myButton = document.getElementById('NZ_skip_button');
 myButton.addEventListener('click', function() {
     myButton.style.display = 'none';
     clickListenerActive.bool = true;
+    myButton.classList.remove("content-anim");
+    myButton.style.opacity = 0.7; //This is a hack will fix later.
 });
