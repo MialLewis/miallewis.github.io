4b6eff859186cc4534865f97d128baa5acd8cddd
Mial Lewis <mial_lewis@live.co.uk>
2024-08-08 03:54:43 +0100

    push broken img reel for test

--- public/travel/src/App.js
+++ public/travel/src/App.js
@@ -1,23 +1,25 @@
 import { useState } from 'react';
 
 const photos = [
-  { src: '../images/CastleView', caption: 'Caption for photo 1' },
-  { src: '../images/CastleWall', caption: 'Caption for photo 2' },
+  { src: '../images/CastleView', heading: 'Pena Palace, Sintra', date: '01/10/2023', caption: 'View from Castelo dos Mouros', alt: 'View of Pena Palace, Sintra' },
+  { src: '../images/CastleWall', heading: 'Castelo dos Mouros, Sintra', date: '01/10/2023', caption: 'Castle walls', alt: 'Walls of Castelo dos Mouros' },
 ];
 
 function App() {
   const [index, setIndex] = useState(0);
   if ( index === 0 ){
-    setIndex(5);
+    setIndex(1);
   }
 
   return (
-    <div>
-      <header>
-        <p className = 'App-header'>
-          The current index number is: {index}
-        </p>
-      </header>
+    <div className = "container container-anim">
+        <div className = "image-txt-container">
+            <img src={photos[index].src} className = "blog-img-ls content-anim" style={{'--end-opacity': 'var(--opaque)'}} alt={photos[index].alt}/> 
+            <div className = "text-block content-anim">
+                <h4>{photos[index].heading}<span style={{'float' : 'right'}}>{photos[index].date}</span></h4>
+                <p>{photos[index].caption}</p>
+            </div>
+        </div>
     </div>
   );
 }
