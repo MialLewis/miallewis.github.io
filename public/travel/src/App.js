d89c61b8d6a3fc064ba70202588a9acfa66098fa
Mial Lewis <mial_lewis@live.co.uk>
2024-08-10 03:09:57 +0100

    add click buttons

--- public/travel/src/App.js
+++ public/travel/src/App.js
@@ -1,24 +1,35 @@
 import { useState } from 'react';
 
 const photos = [
-  { src: '../../images/CastleView', heading: 'Pena Palace, Sintra', date: '01/10/2023', caption: 'View from Castelo dos Mouros', alt: 'Pena Palace, Sintra' },
-  { src: '../../images/CastleWall', heading: 'Castelo dos Mouros, Sintra', date: '01/10/2023', caption: 'Castle walls', alt: 'Walls of Castelo dos Mouros' },
+  { src: '../../images/CastleView', heading: 'Pena Palace, Sintra', date: '01/10/2023', caption: 'View from Castelo dos Mouros', alt: 'Pena Palace, Sintra', class: 'blog-img-ls content-anim' },
+  { src: '../../images/CastleWall', heading: 'Castelo dos Mouros, Sintra', date: '01/10/2023', caption: 'Castle walls', alt: 'Walls of Castelo dos Mouros', class: 'blog-img-ls content-anim' },
 ];
 
 function App() {
   const [index, setIndex] = useState(0);
-  if ( index === 0 ){
-    setIndex(1);
-  }
+  const onClickLeft = () => {
+    if (index == 0) {
+      setIndex(photos.length - 1)
+    } else {
+      setIndex(index - 1)
+    }
+  };
+  const onClickRight = () => {
+    if (index == photos.length - 1) {
+      setIndex(0)
+    } else {
+      setIndex(index + 1)
+    }
+  };
 
   return (
     <div className = "container container-anim">
         <div className = "image-txt-container">
             <div className = "nav-button-container content-anim">
-                <img src='../../assets/left-arrow.png' className = "nav-button" style={{'left': '10px'}} alt="left photo navigation button"/>
-                <img src='../../assets/right-arrow.png' className = "nav-button" style={{'float' : 'right', 'right': '10px'}} alt="right photo navigation button"/>
+                <img src='../../assets/left-arrow.png' className = "nav-button" style={{'left': '10px'}} alt="left photo navigation button" onClick={onClickLeft}/>
+                <img src='../../assets/right-arrow.png' className = "nav-button" style={{'float' : 'right', 'right': '10px'}} alt="right photo navigation button" onClick={onClickRight}/>
             </div>
-            <img src={photos[index].src} className = "blog-img-ls content-anim" style={{'--end-opacity': 'var(--opaque)'}} alt={photos[index].alt}/> 
+            <img src={photos[index].src} className = {photos[index].class} style={{'--end-opacity': 'var(--opaque)'}} alt={photos[index].alt}/> 
             <div className = "text-block content-anim">
                 <h4>{photos[index].heading}<span style={{'float' : 'right'}}>{photos[index].date}</span></h4>
                 <p>{photos[index].caption}</p>
