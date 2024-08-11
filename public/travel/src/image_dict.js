21f492377ea95bfe6146e21cf395f2de2b736813
Mial Lewis <mial_lewis@live.co.uk>
2024-08-11 10:49:29 +0100

    mark photos portrait

--- public/travel/src/image_dict.js
+++ public/travel/src/image_dict.js
@@ -6,9 +6,9 @@ const photos = [
     { src: '../../images/BristolBalloon', heading: 'Ashton Court, Bristol', date: '11/08/2023', caption: 'Balloon fiesta over central Bristol', alt: 'Bristol balloon fiesta from Ashton Court', class: 'blog-img-ls content-anim' },
     { src: '../../images/New Zealand/Jan/001_airbnb_view.jpg', heading: 'Grey Lynn, Auckland', date: '02/01/2024', caption: 'View from Airbnb', alt: 'View from Auckland airbnb', class: 'blog-img-p content-anim' },
     { src: '../../images/New Zealand/Jan/002_mnt_eden.jpg', heading: 'Mount Eden, Auckland', date: '03/01/2024', caption: 'View from summit of Mount Eden', alt: 'View from mount Eden', class: 'blog-img-ls content-anim' },
-    { src: '../../images/New Zealand/Jan/003_auck_beach.jpg', heading: 'Hamilton Beach Reserve, Auckland', date: '03/01/2024', caption: 'Gemma on Hamilon Beach Reserve', alt: 'Auckland Beach', class: 'blog-img-ls content-anim' },
+    { src: '../../images/New Zealand/Jan/003_auck_beach.jpg', heading: 'Hamilton Beach Reserve, Auckland', date: '03/01/2024', caption: 'Gemma on Hamilon Beach Reserve', alt: 'Auckland Beach', class: 'blog-img-p content-anim' },
     { src: '../../images/New Zealand/Jan/004_new_van.jpg', heading: 'Grey Lynn, Auckland', date: '04/01/2024', caption: 'New mini camper van: Viv the Toyota Voxy', alt: 'Toyota Voxy', class: 'blog-img-ls content-anim' },
-    { src: '../../images/New Zealand/Jan/005_hot_w_bay.jpg', heading: 'Kohi Cafe, Mission Bay, Auckland', date: '04/01/2024', caption: 'A "rare" brunch at Kohi Cafe', alt: 'Cafe at Mission Bay', class: 'blog-img-ls content-anim' },
+    { src: '../../images/New Zealand/Jan/005_hot_w_bay.jpg', heading: 'Kohi Cafe, Mission Bay, Auckland', date: '04/01/2024', caption: 'A "rare" brunch at Kohi Cafe', alt: 'Cafe at Mission Bay', class: 'blog-img-p content-anim' },
     { src: '../../images/New Zealand/Jan/006_auck_classic.jpg', heading: 'ASB Tennis Centre, Auckland', date: '04/01/2024', caption: 'Raducanu v Svitolina, ASB Classic', alt: 'Tennis match, Auckland Classic', class: 'blog-img-ls content-anim' }
   ];
 export { photos };
